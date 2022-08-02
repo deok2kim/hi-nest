c38f367050f24bb53989f6fa6fc43ae953c407f6
deokgi kim <dukgi77@naver.com>
2022-08-02 20:24:08 +0900

    feat: controllers and services"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
+// 데코레이터는 클래스에 함수 기능을 추가할 수 있다.
+// app.modeule 은 루트모듈
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [], // url 을 가져오고 함수를 실행, express 의 라우터 역할
+  providers: [], // 서비스로직, 구조와 아키텍쳐, 컨트롤러와 비지니스 로직 분리
 })
 export class AppModule {}
